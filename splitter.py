747cb395ef6f16bad2a6bad1f88db6c6ab622974
Gareth Hay <gazhay@gmail.com>
2020-06-10 15:23:42 +0100

    pythonanywhere sucks

--- splitter.py
+++ splitter.py
@@ -1,20 +1,6 @@
 import textwrap
 
-testStr = """Keir Starmer: "It shouldn't have been done in that way. It's completely wrong to pull a
-statue down like that but stepping back, that statue should have been taken down a long,
-long time ago.
-can't in 21st Century Britain have a slaver on a statue — a statue is there to honour
-people.
-can't have that in 21st Century Britain. That statue should have been brought down
-properly with consent and put, I would say, in a museum.
-"This was a man who was responsible for 100,000 people being moved from Africa to the
-Caribbean as slaves, including women and children, who were branded on their chests
-with the name of the company that he ran.
-"Of the 100,000, 20,000 died en route and they were chucked in the sea. He should not be
-on a statue in Bristol or anywhere else.
-"He should be in a museum because we need to understand thk but it should have been
-taken down a long time ago."
-"""
+testStr = ""
 
 class Splitter:
 
